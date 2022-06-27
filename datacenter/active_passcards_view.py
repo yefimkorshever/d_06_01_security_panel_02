5ffb9331f245cdaec6bdc4498e629c37be0a170d
yk <yefim_korshever@mail.ru>
2022-06-27 20:32:46 +0300

    Remove unused imports

--- datacenter/active_passcards_view.py
+++ datacenter/active_passcards_view.py
@@ -1,5 +1,4 @@
 from datacenter.models import Passcard
-from datacenter.models import Visit
 from django.shortcuts import render
 
 
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,4 +1,3 @@
-from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
 from django.utils.timezone import localtime
